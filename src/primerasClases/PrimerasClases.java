82affb0dde684daa5e06d6b1d5a43d3315638c0d
JABALOC <jabalo1988@gmail.com>
2025-08-08 17:50:07 +0200

    Curso de Java

---
+++ src/primerasClases/PrimerasClases.java
@@ -0,0 +1,10 @@
+package primerasClases;
+
+public class PrimerasClases {
+    public static void main (String[] args) {
+        int edad;
+        edad = 37;
+
+        System.out.println(edad);
+    }
+}
